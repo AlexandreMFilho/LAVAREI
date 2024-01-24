7f4862864608e71c9c51327c6d5c9b263673ace4
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2024-01-24 13:24:10 -0300

    detectando o hit, agora identificar a fatia para mostrar o hover

--- graficos/sandboxConcretada/js.js
+++ graficos/sandboxConcretada/js.js
@@ -41,28 +41,57 @@ function pedido(fat){
 
 function desenha(pizza){
   var layer = new Concrete.Layer();
+  var hover = new Concrete.Layer(); 
+  
   layer.visible = true;
+  hover.visible = false;
+  
   view.add(layer);
+  view.add(hover);
+  
   ctx = layer.scene.context;
+  hv = hover.scene.context;
+  hit = layer.hit.context;
+
   for(var i = 1; i < pizza.length; i++){
     var x = pizza[i].offset;                                        
     var y = pizza[i].offset + pizza[i].valor;                        
-    ctx.fillStyle = pizza[i].cor; //seta a cor da fatia
-  
+    
     //Desenha a fatia
     ctx.beginPath();
     ctx.moveTo(cx, cy);
     ctx.arc(cx,cy,r,degtorad(x)*3.6,degtorad(y)*3.6,false);
     ctx.lineTo(cx, cy);
     ctx.fill();
+
+    hit.beginPath();
+    hit.moveTo(cx, cy);
+    hit.arc(cx,cy,r,degtorad(x)*3.6,degtorad(y)*3.6,false);
+    hit.lineTo(cx, cy);
+    hit.fill();
+    
+    hv.beginPath();
+    hv.moveTo(cx, cy);
+    hv.arc(cx,cy,r+1,degtorad(x)*3.6,degtorad(y)*3.6,false);
+    hv.lineTo(cx, cy);
+    hv.stroke();
+    hv.strokeStyle = 'green';
+    hv.lineWidth = 3;
     
     
     //Desenha o bloco da legenda
     ctx.rect(200, 20*(i), 10, 10);
     ctx.fill();
     
-    //Desenha a o bloco da legenda
+    hv.rect(200, 20*(i), 10+2, 10+2);
+    hv.stroke();
+    
+    //Desenha a legenda
+    ctx.fillStyle = pizza[i].cor; //seta a cor da fatia
     ctx.fillText(`${pizza[i].rotulo}: ${pizza[i].valor}%` , 220,10+(20*(i)));
+
+    hv.fillStyle = 'green'; //seta a cor da fatia
+    hv.fillText(`${pizza[i].rotulo}: ${pizza[i].valor}%` , 220,10+(20*(i)));
     
     ctx.closePath();
     // pizza[i].desenho = imagem;
@@ -81,6 +110,21 @@ function cansado(){
   pedido(a);
 }
 
+concreteContainer.addEventListener('mousemove', function(evt) {
+  var boundingRect = concreteContainer.getBoundingClientRect(),
+      x = evt.clientX - boundingRect.left,
+      y = evt.clientY - boundingRect.top,
+      key = view.getIntersection(x, y),
+      hovers = view.Layers;
+      console.log(key);
+      
+      // unhover all circles
+  for(var i = 0; i < hovers.length; i+=2){
+    hovers[i+1].visible = false;
+  }
+});
+
 // console.log(`pizza:${pizza}`);
 // console.log(`view:${view.layers}`);
+
 
